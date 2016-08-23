10dfa195b9259b5ca85191f30f9673775ecc5190
Mark Lybrand <mlybrand@gmail.com>
2016-08-22 19:34:03 -0700

    committing uncommitted content to see slack integration

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # iifdb
 Internet International Football Database
+
+Planned technology:
+
+* Angular 2
+* Posgresql
+* Node.js
+* Express.js
+
---
+++ wallaby.js
@@ -0,0 +1,10 @@
+module.exports = function (wallaby) {
+  return {
+    files: [
+      'src/**/*.js'
+    ],
+    tests: [
+      'test/**/*Spec.js'
+    ]
+  };
+};
